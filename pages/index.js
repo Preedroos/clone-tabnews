b7e0715455d510ec8d2dcdd9124588f7039447ad
Pedro Paulo Fagundes Cabral <69660117+Preedroos@users.noreply.github.com>
2025-12-28 20:31:24 +0000

    Add pages folder

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Hello Worlds!</h1>
+            <p>I'm hearing something getting out my laptop...</p>
+        </div>
+    )
+}
+
+export default Home;
