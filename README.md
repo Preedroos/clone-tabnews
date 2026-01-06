fec8abcf3e9bfab7ff2aa61673a5ea2ea4b0bc7f
Pedro Paulo Fagundes Cabral <69660117+Preedroos@users.noreply.github.com>
2026-01-06 00:00:38 +0000

    Add `lint:check` and `lint:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do clone do https://tabnews.com.br como exercício de programação.
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
-    return (
-        <div>
-            <h1>Hello Worlds!</h1>
-            <p>I'm hearing something getting out my laptop...</p>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Hello Worlds!</h1>
+      <p>I'm hearing something getting out my laptop...</p>
+    </div>
+  );
 }
 
 export default Home;
